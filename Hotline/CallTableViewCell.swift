cb8434394d1683afd86ffc4346e71f962578ac40
beepscore <support@beepscore.com>
2017-10-26 23:22:44 -0700

    Re-indent

--- Hotline/CallTableViewCell.swift
+++ Hotline/CallTableViewCell.swift
@@ -23,38 +23,38 @@
 import UIKit
 
 class CallTableViewCell: UITableViewCell {
-  
-  var callState: CallState? {
-    didSet {
-      guard let callState = callState else { return }
-      
-      switch callState {
-      case .active:
-        callStatusLabel.text = "Active"
-      case .held:
-        callStatusLabel.text = "On Hold"
-      case .connecting:
-        callStatusLabel.text = "Connecting..."
-      default:
-        callStatusLabel.text = "Dialing..."
-      }
-    }
-  }
-  
-  var incoming: Bool = false {
-    didSet {
-      // statement uses image literals for incoming_arrow and outgoing_arrow
-      iconImageView.image = incoming ? #imageLiteral(resourceName: "incoming_arrow") : #imageLiteral(resourceName: "outgoing_arrow")
-    }
-  }
-  
-  var callerHandle: String? {
-    didSet {
-      callerHandleLabel.text = callerHandle
-    }
-  }
 
-  @IBOutlet private var iconImageView: UIImageView!
-  @IBOutlet private var callerHandleLabel: UILabel!
-  @IBOutlet private var callStatusLabel: UILabel!
+    var callState: CallState? {
+        didSet {
+            guard let callState = callState else { return }
+
+            switch callState {
+            case .active:
+                callStatusLabel.text = "Active"
+            case .held:
+                callStatusLabel.text = "On Hold"
+            case .connecting:
+                callStatusLabel.text = "Connecting..."
+            default:
+                callStatusLabel.text = "Dialing..."
+            }
+        }
+    }
+    
+    var incoming: Bool = false {
+        didSet {
+            // statement uses image literals for incoming_arrow and outgoing_arrow
+            iconImageView.image = incoming ? #imageLiteral(resourceName: "incoming_arrow") : #imageLiteral(resourceName: "outgoing_arrow")
+        }
+    }
+
+    var callerHandle: String? {
+        didSet {
+            callerHandleLabel.text = callerHandle
+        }
+    }
+
+    @IBOutlet private var iconImageView: UIImageView!
+    @IBOutlet private var callerHandleLabel: UILabel!
+    @IBOutlet private var callStatusLabel: UILabel!
 }
