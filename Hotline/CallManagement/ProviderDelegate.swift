11713ff905fb0061516885fe24b034b01d432af0
beepscore <support@beepscore.com>
2017-10-27 16:18:30 -0700

    Add code from tutorial

--- Hotline/CallManagement/ProviderDelegate.swift
+++ Hotline/CallManagement/ProviderDelegate.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2017 Razeware LLC. All rights reserved.
 //
 
-import Foundation
+import AVFoundation
+import CallKit
+
+class ProviderDelegate: NSObject {
+    // 1.
+    fileprivate let callManager: CallManager
+    fileprivate let provider: CXProvider
+
+    init(callManager: CallManager) {
+        self.callManager = callManager
+        // 2.
+        provider = CXProvider(configuration: type(of: self).providerConfiguration)
+
+        super.init()
+        // 3.
+        provider.setDelegate(self, queue: nil)
+    }
+
+    // 4.
+    static var providerConfiguration: CXProviderConfiguration {
+        let providerConfiguration = CXProviderConfiguration(localizedName: "Hotline")
+
+        providerConfiguration.supportsVideo = true
+        providerConfiguration.maximumCallsPerCallGroup = 1
+        providerConfiguration.supportedHandleTypes = [.phoneNumber]
+
+        return providerConfiguration
+    }
+}
