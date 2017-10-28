0db376cde5ce1d87cb3f0a1549219cdf7f70d963
beepscore <support@beepscore.com>
2017-10-27 20:15:45 -0700

    Re-indent

--- Hotline/CallManagement/CallManager.swift
+++ Hotline/CallManagement/CallManager.swift
@@ -24,35 +24,35 @@ import Foundation
 import CallKit
 
 class CallManager {
-  
-  var callsChangedHandler: (() -> Void)?
-  
-  private(set) var calls = [Call]()
-  
-  func callWithUUID(uuid: UUID) -> Call? {
-    guard let index = calls.index(where: { $0.uuid == uuid }) else {
-      return nil
+
+    var callsChangedHandler: (() -> Void)?
+
+    private(set) var calls = [Call]()
+
+    func callWithUUID(uuid: UUID) -> Call? {
+        guard let index = calls.index(where: { $0.uuid == uuid }) else {
+            return nil
+        }
+        return calls[index]
     }
-    return calls[index]
-  }
-  
-  func add(call: Call) {
-    calls.append(call)
-    call.stateChanged = { [weak self] in
-      guard let strongSelf = self else { return }
-      strongSelf.callsChangedHandler?()
+
+    func add(call: Call) {
+        calls.append(call)
+        call.stateChanged = { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.callsChangedHandler?()
+        }
+        callsChangedHandler?()
     }
-    callsChangedHandler?()
-  }
-  
-  func remove(call: Call) {
-    guard let index = calls.index(where: { $0 === call }) else { return }
-    calls.remove(at: index)
-    callsChangedHandler?()
-  }
-  
-  func removeAllCalls() {
-    calls.removeAll()
-    callsChangedHandler?()
-  }
+
+    func remove(call: Call) {
+        guard let index = calls.index(where: { $0 === call }) else { return }
+        calls.remove(at: index)
+        callsChangedHandler?()
+    }
+
+    func removeAllCalls() {
+        calls.removeAll()
+        callsChangedHandler?()
+    }
 }
